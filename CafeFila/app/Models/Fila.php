a68d7c163b46168da24a232aef7e650c22219a4f
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-11-05 21:25:18 -0300

    usuario arrumado

---
+++ CafeFila/app/Models/Fila.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fila extends Model
+{
+    protected $database = "fila";
+
+    protected $primaryKey = "id";
+
+    public $timestamps = false;
+}
